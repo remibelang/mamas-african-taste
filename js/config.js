8041fe54f91bd7e0cc012c091b7fceea0298e28d
remibelang <belangremi@gmail.com>
2026-02-18 23:47:03 -0500

    Remove duplicate config.js references

--- js/config.js
+++ js/config.js
@@ -1,27 +1,28 @@
 // API Configuration - switches between local and production
-const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-    ? 'http://localhost:5000'
-    : 'https://mamas-african-taste-api.onrender.com';
+var API_BASE_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+    ? "http://localhost:5000"
+    : "https://mamas-african-taste-api.onrender.com";
 
 // Helper function for API calls
-async function apiCall(endpoint, options = {}) {
-    const url = API_BASE_URL + endpoint;
-    const defaultOptions = {
+async function apiCall(endpoint, options) {
+    options = options || {};
+    var url = API_BASE_URL + endpoint;
+    var defaultOptions = {
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json"
         }
     };
     
-    const token = localStorage.getItem('adminToken');
+    var token = localStorage.getItem("adminToken");
     if (token) {
-        defaultOptions.headers['Authorization'] = 'Bearer ' + token;
+        defaultOptions.headers["Authorization"] = "Bearer " + token;
     }
     
-    const response = await fetch(url, { ...defaultOptions, ...options });
+    var response = await fetch(url, Object.assign({}, defaultOptions, options));
     return response;
 }
 
-async function apiCallJson(endpoint, options = {}) {
-    const response = await apiCall(endpoint, options);
+async function apiCallJson(endpoint, options) {
+    var response = await apiCall(endpoint, options);
     return response.json();
 }
